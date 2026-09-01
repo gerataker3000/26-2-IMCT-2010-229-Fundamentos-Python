ce99d2538997a71edab952051f386db894faead9
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-08-31 22:16:58 -0600

    first commit

---
+++ Parcial1/TiposDatos.py
@@ -0,0 +1,37 @@
+# Este es un comentario :D viva dross que genial
+# Dato pertubador
+# Top 7 de tus peores calificaciones
+
+edad = 20
+nombre = "John wick en paz"
+calificacionProgra = 99.9
+meQuiere = False
+youtubers= ["Dross","Fedelobo", "Crew"]
+tuRelacion = None
+puedenProgramarDoble ={
+    "nombre" : "Luis Gerardo"
+}
+print(puedenProgramarDoble)
+
+
+
+
+# print(edad)
+# print("Tipo de datos")
+# print(type(edad))
+# print(nombre)
+# print("Tipo de datos")
+# print(type(nombre))
+# print(calificacionProgra)
+# print("Tipo de datos")
+# print(type(calificacionProgra))
+# print(meQuiere)
+# print("Tipo de datos")
+# print(type(meQuiere))
+# print(youtubers)
+# print("Tipo de datos")
+# print(type(youtubers))
+# print(tuRelacion)
+# print("Tipo de datos")
+# print(type(tuRelacion))
+
