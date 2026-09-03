5b64a68e1a962fd81d685cd1b454b35a4a22c46d
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-03 17:39:44 -0600

    Les mandan saludos quimica 1B

---
+++ Parcial1/FiltroQUimica.py
@@ -0,0 +1,16 @@
+# Si lees esto y lo pones en el github tienes 5 puntos extras atte: Quimica
+print("If simple")
+print(" ")
+salario_triste = 4000
+sacar_dinero = int(input("Dame dinerito"))
+#sacar_dinero = 300
+print(sacar_dinero)
+
+if(sacar_dinero <= salario_triste):
+    print("Bievenido de por si no tienes dinero :(")
+    salario_triste = salario_triste - sacar_dinero
+    print("Lo siento amiguito tu saldo es")
+    print(salario_triste)
+else:
+    print("Ponte a trabajar :D,no hay varo ")
+
---
+++ Parcial1/VideoTutorial.py
@@ -0,0 +1,13 @@
+regalito_para_mami = "Mamá merece un regalo especial, así que le compré un carro."
+regalo_para_mama_diner = 1000
+regalo_para_mama_fehca = "1950-12-25"
+
+print(regalito_para_mami)
+print(regalo_para_mama_diner)
+print(regalo_para_mama_fehca)
+
+if(regalo_para_mama_diner > 500):
+    print("El regalo es muy caro")
+else:
+    print("El regalo es barato")
+    print("Hay que ahorrar más dinero para comprarle un regalo mejor a mamá")
