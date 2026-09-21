15c3d6f09e92f9a6cba364cec9fcd99e38e8b465
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-21 15:50:43 -0600

    Colecciones en pyhton

--- index.py
+++ index.py
@@ -1,1 +1,12 @@
 print("Hola mundo")
+
+for i in range(5):
+    print(f"Iteración {i}")
+
+
+nombre ="nombre"
+
+for i, letra in enumerate(nombre):
+    print(f"Iteración {i}: {letra}")
+
+print(nombre[0])
